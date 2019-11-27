86ddbbf6f006fa194893feccf787465bfc8d9430
ondrej17 <bily.ondrej1701@gmail.com>
2019-11-27 21:16:51 +0100

    Game object and Window object created. Loaded background in playground

--- src/game.py
+++ src/game.py
@@ -1,19 +1,54 @@
-import tkinter
+import tkinter as tk
 
-class Program:
+
+class Game:
+    # dimension of window
+    width_of_left = 600
+    width_of_right = 200
+    height = 960
+
     def __init__(self):
-        # don't change canvas dimensions
-        self.canvas = tkinter.Canvas(width=800, height=960, bg='black')
-        self.canvas.pack()
+        # at the initialization create root object and window
+        self.root = tk.Tk()
+        self.root.title("Porygon")
+        self.root.geometry("{}x{}".format(Game.width_of_left + Game.width_of_right, Game.height))
 
-        self.background = tkinter.PhotoImage(file='background.png')
-        self.canvas.create_image(300, 480, image=self.background)
-        tkinter.mainloop()
+        self.window = Window(self.root)
+
+        self.root.mainloop()
+
+    # TODO: other methods for Game object
 
 
-Program()
+class Window:
 
-# creating different types of ball
-# ball_blue = tkinter.PhotoImage(file='marble_blue.png')
-# ball_yellow = tkinter.PhotoImage(file='marble_yellow.png')
-# ball_red = tkinter.PhotoImage(file='marble_red.png')
+    def __init__(self, root):
+        self.root = root
+        self.main_frame = tk.Frame(self.root)
+        self.main_frame.pack()
+
+        # create right frame that contains buttons, high-score, etc.
+        self.right_frame = tk.Frame(self.main_frame,
+                                    bg='grey',
+                                    width=Game.width_of_right,
+                                    height=Game.height)
+
+        # create canvas aka playground
+        self.playground = tk.Canvas(self.main_frame,
+                                    bg='pink',
+                                    width=Game.width_of_left,
+                                    height=Game.height)
+
+        # pack playground and right frame to main_frame
+        self.right_frame.pack(side='right')
+        self.playground.pack(side='left')
+
+        # add background to playground
+        self.image = tk.PhotoImage(file='../images/background.png')
+        self.background = self.playground.create_image(Game.width_of_left/2, Game.height/2, image=self.image)
+
+        # TODO: add stuff to the right frame
+
+
+# run Porygon
+Game()
